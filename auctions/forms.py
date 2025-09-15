3c89ef88545186853d75ab5ddf7840bfde3d9373
davidjs1212 <davidjs1212@gmail.com>
2025-09-15 11:20:17 -0400

    Added styling and cleaned codebase.

--- auctions/forms.py
+++ auctions/forms.py
@@ -6,13 +6,16 @@ class ListingForm(forms.ModelForm):
     class Meta:
         model = Listing
         fields = ["title", "description", "starting_bid", "photo", "category"]
-
         widgets = {
-            "title": forms.TextInput(attrs={"class": "form-control", "placeholder": "Enter a title"}),
+            "title": forms.TextInput(attrs={"class": "form-control", "placeholder": "Enter a title", "style": "max-width:400px",}),
             "description": forms.Textarea(attrs={"class": "form-control", "rows":8, "placeholder": "Enter a description"}),
-            "starting_bid": forms.NumberInput(attrs={"class": "form-control", "step": "0.01","placeholder": "$0.00"}),
-            "photo": forms.URLInput(attrs={"class": "form-control", "placeholder": "Optional"}),
-            "category": forms.TextInput(attrs={"class": "form-control", "placeholder": "Optional"}),
+            "starting_bid": forms.NumberInput(attrs={"class": "form-control", "step": "0.01","placeholder": "$0.00", "style": "max-width:150px",}),
+            "photo": forms.URLInput(attrs={"class": "form-control", "placeholder": "Optional", "style": "max-width:400px",}),
+            "category": forms.TextInput(attrs={"class": "form-control", "placeholder": "Optional", "style": "max-width:400px",}),
+        }
+
+        labels = {
+            "photo": "Photo URL"
         }
 
 
@@ -20,9 +23,13 @@ class BidForm(forms.ModelForm):
     class Meta:
         model = Bid
         fields = ["amount"]
-
         widgets = {
-            "amount": forms.NumberInput(attrs={"class": "form-control", "step": "0.01", "placeholder": "Enter your bid"})
+            "amount": forms.NumberInput(attrs={
+                "class": "form-control", 
+                "step": "0.01", 
+                "placeholder": "Enter your bid",
+                "style": "max-width:150px",
+            })
         }
         labels = {
             "amount": "Your Bid ($)"
