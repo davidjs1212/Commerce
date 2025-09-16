697f48fa45b20b8d08de2a6f723d794fd4abe7d0
davidjs1212 <davidjs1212@gmail.com>
2025-09-16 19:41:45 -0400

    Changed thumbnail styling and cleaned codebase.

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 from .models import User, Listing, Bid, Comment
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
 
@@ -19,7 +20,7 @@ class CommentAdmin(admin.ModelAdmin):
     list_display = ("listing", "author", "created_at")
     search_fields = ("listing__title", "author__username")
 
-admin.site.register(User)
+admin.site.register(User, UserAdmin)
 admin.site.register(Bid, BidAdmin)
 admin.site.register(Comment, CommentAdmin)
 admin.site.register(Listing, ListingAdmin)
